21a4e13b8a4021552df537198875563e6ca9ccbe
Adilson Luz <adilson.luz@fatec.sp.gov.br>
2021-09-03 20:34:26 -0300

    aula11
    01

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 70.5
+
+console.log(`A velocidade de seu carro é ${vel}km/h`)
+
+if(vel > 60){
+    console.log('Voce ultrapassou os limites de velocidade, MULTADO')
+}
+console.log('Dirija sempre usando cinto segurança')
