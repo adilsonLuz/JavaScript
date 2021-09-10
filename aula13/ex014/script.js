ad4f61c33fad41ed13887cc7b5b247bd969f9baa
Adilson Luz <adilson.luz@fatec.sp.gov.br>
2021-09-09 21:22:09 -0300

    Aula 12 exercicios
    v01

---
+++ aula13/ex014/script.js
@@ -0,0 +1,24 @@
+function carregar() {
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+
+var data = new Date
+
+var hora = 20//data.getHours()
+var min = 30//data.getMinutes()
+var sec = 22//data.getSeconds()
+
+msg.innerHTML = `Agora são ${hora}:${min}:${sec}`
+
+if (hora >= 0 && hora < 12) {
+    img.src = 'manha.png'
+    document.body.style.background = '#e2cd9f'
+} else if (hora >= 12 && hora < 18) {
+    img.src = 'tarde.png'
+    document.body.style.background = '#b9846f'
+} else {
+    img.src = 'noite.png'
+    document.body.style.background = '#515154'
+}
+
+}
