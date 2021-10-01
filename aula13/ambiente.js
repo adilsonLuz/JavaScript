6be95c431965ddd972951add0159986e9d1d5733
Adilson Luz <adilson.luz@fatec.sp.gov.br>
2021-10-01 17:39:27 -0300

    13
    v01

---
+++ aula13/ambiente.js
@@ -0,0 +1,39 @@
+var c = 1
+while(c <= 6){
+    console.log('tudo bem?')
+    c++
+}
+
+c = 1
+while(c <= 15){
+    console.log(`WHILE ${c}`)
+    c++
+}
+
+c=1
+do{
+    console.log(`DO WHILE ${c}`)
+    c++
+}while(c <= 10)
+
+for(c=1; c <= 8; c++){
+    console.log(`FOR ${c}`)
+}
+
+
+
+
+
+
+
+
+
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
