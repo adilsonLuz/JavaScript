75bf381750be23e3293b2ec98b207237f7e204b0
Adilson Luz <adilson.luz@fatec.sp.gov.br>
2021-10-02 22:39:48 -0300

    rascunho
    rascunhos

---
+++ prova/prova.js
@@ -0,0 +1,18 @@
+//convertendo para valor monetario brasileiro
+let n = 1543.5
+
+console.log(n.toLocaleString('pt-BR', {style:'currency', currency: 'BRL'}))
+
+//Resulta em 64
+let num = 8
+console.log(num **=2)
+
+//como pode começar as variaveis e funcoes
+let _f = 3
+let $y = 'Jota'
+
+console.log(_f, $y)
+
+//
+
+window.Keyboard()
